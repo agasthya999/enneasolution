36b86d96ac3ba0427d335d0eef57784972b2a4a6
DELL <DELL@DESKTOP-G47OK10>
2022-09-19 18:31:51 +0530

    first commit

---
+++ Inventory_ennea/src/main/java/com/example/Inventory_ennea/InventoryEnneaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Inventory_ennea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryEnneaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryEnneaApplication.class, args);
+	}
+
+}
---
+++ Inventory_ennea/src/test/java/com/example/Inventory_ennea/InventoryEnneaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Inventory_ennea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryEnneaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
